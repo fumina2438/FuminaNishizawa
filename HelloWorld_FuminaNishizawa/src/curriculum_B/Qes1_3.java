0cf2a0efca2a1f70f4e45868dbfae578ee27c0d0
betty <betty@localhost>
2025-04-10 16:42:43 +0900

    curriculumB再提出

--- HelloWorld_FuminaNishizawa/src/curriculum_B/Qes1_3.java
+++ HelloWorld_FuminaNishizawa/src/curriculum_B/Qes1_3.java
@@ -13,22 +13,26 @@ public class Qes1_3 {
 		System.out.println("ユーザー名を入力してください： ");
 		String username = scanner.nextLine();//ユーザーからの入力を受け取る
 
-		username = "superlongname";//文字数が１０文字より大きい場合
-		System.out.println(username.length() > 10 ? "名前を１０文字以内にしてください" : "OK");
-
-		if (username == null || username.length() == 0)//文字数が０文字以下もしくはnullの場合
+		
+		if (username.length() > 10) {//文字数が０文字以下もしくはnullの場合
+			System.out.println("名前を１０文字以内にしてください");
+			return;
+		}
+		if (username == null || username.length() == 0) {//文字数が０文字以下もしくはnullの場合
 			System.out.println("名前を入力してください");
+			return;
+		}
 
-		if (username == null || username.trim().isEmpty())//正常な値だった場合
-			System.out.println("ユーザー名「" + username + "」を登録しました");
-
-		if (!username.matches("[a-zA-Z0-9]+"))
+		if (!username.matches("[a-zA-Z0-9]+")) {
 			System.out.println("半角英数字のみで名前を入力してください");
-
+			return;
+		}
+			System.out.println("ユーザー名「" + username + "」を登録しました");
+		
 		String[] hand = { "グー", "チョキ", "パー" };
 		scanner = new Scanner(System.in);
 		Random random = new Random();
-
+        int count = 0;
 		while (true) {
 			System.out.print("グー・チョキ・パー　から選んでください:");
 			String playerInput = scanner.nextLine();
@@ -38,6 +42,7 @@ public class Qes1_3 {
 				System.out.println("無効な入力です。もう一度。");
 				continue;
 			}
+			count++;
 			int cpu = random.nextInt(3);
 			System.out.println("コンピュータ：" + hand[cpu]);
 
@@ -47,15 +52,14 @@ public class Qes1_3 {
 				System.out.println("あなたの勝ち！\n");
 				System.out.println("やるやん。\n次は俺にリベンジさせて");
 				break;
-				//グーで負けた場合（プレイヤーがグーでCPUパー）
-
-			} else if (player == 0 && cpu == 2) {
+				//グーに負けた場合（プレイヤーがチョキでCPUグー）
+			} else if (player == 1 && cpu == 0) {
 				System.out.println("俺の勝ち！");
 				System.out.println("負けは次に繋がるチャンスです！");
 				System.out.println("ネバーギブアップ！");
 
 				//チョキに負けた場合（プレイヤーがパーでCPUがチョキ）
-			} else if (player == ２ && cpu == 1) {
+			} else if (player == 2 && cpu == 1) {
 				System.out.println("俺の勝ち！");
 				System.out.println("たかがじゃんけん、そう思わないですか？");
 				System.out.println("そうやったら次も、俺が勝ちますよ");
@@ -73,12 +77,7 @@ public class Qes1_3 {
 				System.out.println("あなたの負け…\n");
 			}
 		}
-		//じゃんけん処理
-		int count = 0;
-		boolean win = false;
-		while (!win) {
-			count++;
-		}
+		
 		System.out.printf("勝つまでにかかった合計回数は" + count + "回です");
 		scanner.close();
 
